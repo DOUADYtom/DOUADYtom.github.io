5c49533e465f175f4ff31885b01e13ab88277f73
DOUADYtom <douady.tom@gmail.com>
2020-11-03 11:56:28 +0100

    Reseponsive v1

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var catEl = document.getElementById("cat");
-  
-var startTime = new Date().getTime();
-var walkTheCat = function() {
-    var currTime = new Date().getTime();
-    var secondsElapsed = ((currTime - startTime)/1000);
-};
-walkTheCat();
